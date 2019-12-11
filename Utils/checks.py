8db64dbdf63705db2ff562fca54423ce184169b6
James <50501825+Gobot1234@users.noreply.github.com>
2019-12-11 22:12:39 +0000

    Add files via upload

---
+++ Utils/checks.py
@@ -0,0 +1,44 @@
+from discord.ext.commands import CommandError
+
+
+def prefix(ctx):
+    # select ctx.guild.id return prefix
+    pass
+
+
+def colour_good(ctx):
+    # select ctx.guild.id return colour
+    pass
+
+
+def colour_bad(ctx):
+    # select ctx.guild.id return colour
+    pass
+
+
+def colour_neutral(ctx):
+    # select ctx.guild.id return colour
+    pass
+
+
+def is_guild_owner(ctx):
+    if ctx.author == ctx.guild.owner:
+        return
+    else:
+        raise NotGuildOwner(
+            f'You are not the owner of this guild contact {ctx.guild.owner} if a command needs to be performed')
+
+
+def is_agsb_guild(ctx):
+    if ctx.guild.id == 376064130118844416:
+        return
+    else:
+        raise NotAGSBSever('You are not in the AGSB server so this command cannot be used')
+
+
+class NotGuildOwner(CommandError):
+    """Custom Exception class for Guild Owner Commands"""
+
+
+class NotAGSBSever(CommandError):
+    """Custom Exception class to check if its the AGSB server"""
