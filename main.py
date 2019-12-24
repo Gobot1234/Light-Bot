e8abf165ec12f7c2c3ee583eab5a70029a7c0696
James <50501825+Gobot1234@users.noreply.github.com>
2019-12-24 11:32:52 +0000

    Updated to subclassed

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
+import asyncio
 from asyncpg import create_pool
 from os import listdir
 from os.path import isfile, join
 from sys import stderr
 from traceback import print_exc
 from datetime import datetime
-
 import config
 from discord.ext.commands import Bot, when_mentioned_or
-import jishaku
+from logging import getLogger, DEBUG, Formatter, FileHandler
+#import jishaku
+
+from Utils.paginator import Paginator
 
 cogs_dir = "Cogs"
-
-# async def run():
-    # bot.pg_con = await create_pool(database='postgres', user='postgres', password=config.DBPassword)
 
 
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
-
-    # Notice how you can use spaces in prefixes. Try to keep them simple though. lul
-    prefixes = ['=']
-
+    #prefixes = bot.prefixes[message.guild.id]
+    prefix = '='
     # Check to see if we are outside of a guild. e.g DM's etc.
     if message.guild is None:
-        # Only allow '' or ! to be used in DMs
         return ''
-    return when_mentioned_or(*prefixes)(bot, message)
+    return when_mentioned_or(prefix)(bot, message)
 
 
-bot = Bot(command_prefix=get_prefix, case_insensitive=True,
-          description='**Epic Bot = best bot**\nWhen using command with args, `<>` indicates a required argument and '
-                      '`[]` indicates an optional argument.\nDon\'t however type these around your arguments')
+class EpicBot(Bot):
+    def __init__(self):
+        bot = super().__init__(command_prefix=get_prefix, case_insensitive=True)
+        self.bot = bot
 
-bot.initial_extensions = [f[:-3] for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]
-# getting the cog files in the "Cogs" folder and removing the none .py ones
+    def run(self, bot):
+        bot.initial_extensions = [f[:-3] for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]
+        # getting the cog files in the "Cogs" folder and removing the none .py ones
+        print(f'Extensions to be loaded are {bot.initial_extensions}')
+        for extension in bot.initial_extensions:
+            try:
+                bot.load_extension(f'Cogs.{extension}')
+            except Exception as e:
+                print(f'Failed to load extension {extension}. {e}', file=stderr)
+                print_exc()
+        #bot.load_extension('jishaku')
+        bot.launch_time = datetime.utcnow()
+        super().run(config.token)
+
+    async def on_message(self, message):
+        ctx = await self.get_context(message, cls=Paginator)
+        await self.invoke(ctx)
+
+
+def run_bot():
+    bot = EpicBot()
+    loop = asyncio.get_event_loop()
+    '''
+    try: #https://gist.github.com/jegfish/cfc7b22e72426f5ced6f87caa6920fd6
+        bot.pool = bot.loop.run_until_complete(create_pool(database='postgres', user='postgres', password=config.DBPassword, command_timeout=60))
+    except Exception as e:
+        bot.log.exception('Could not set up PostgreSQL. Exiting.')
+    '''
+    #print(bot.pool)
+    bot.run(bot)
+
 
 if __name__ == '__main__':
-    print(f'Extensions to be loaded are {bot.initial_extensions}')
-    for extension in bot.initial_extensions:
-        try:
-            bot.load_extension(f'Cogs.{extension}')
-        except Exception as e:
-            print(f'Failed to load extension {extension}.', file=stderr)
-            print_exc()
-    bot.load_extension('jishaku')
-    bot.launch_time = datetime.utcnow()
-
-bot.run(config.Token)
+    bot = EpicBot()
+    bot.log = getLogger()
+    bot.log.setLevel(DEBUG)
+    handler = FileHandler(filename='out.log', encoding='utf-8', mode='w')
+    handler.setFormatter(Formatter('%(asctime)s : %(levelname)s : %(name)s | %(message)s'))
+    bot.log.addHandler(handler)
+    run_bot()
